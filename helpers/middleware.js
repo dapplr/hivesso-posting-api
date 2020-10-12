ca312d1338db92e3a8a919983e23eb1514f0fd85
Dinesh Yadav <dinesh.yadav@loconav.com>
2020-10-13 00:15:26 +0530

    Updated Authentication method

--- helpers/middleware.js
+++ helpers/middleware.js
@@ -1,9 +1,7 @@
-import { createHash } from 'crypto';
-import { intersection, has } from 'lodash';
-import { verify } from './token';
-import { getAppProfile, b64uToB64 } from './utils';
-import client from './client';
-import { authorized_operations } from '../config.json';
+const client = require('./client');
+const config = require('../config.json');
+const jwt = require('jsonwebtoken');
+const lodash = require('lodash');
 
 /**
  * Check if user allow app proxy account to post on his behalf
@@ -17,28 +15,20 @@ export const verifyPermissions = async (req, res, next) => {
     console.error('Unable to load accounts from hived', req.proxy, req.user, e);
   }
 
-  if (!has(accounts, '[0].name') || !has(accounts, '[1].name')) {
+  if (!lodash.has(accounts, '[0].name') || !lodash.has(accounts, '[1].name')) {
     res.status(401).json({
       error: 'unauthorized_client',
       error_description: `The app @${req.proxy} or user @${req.user} account failed to load`,
     });
   } else {
     const userAccountAuths = accounts[1].posting.account_auths.map((account) => account[0]);
-    if (userAccountAuths.indexOf(req.proxy) === -1) {
+    if (userAccountAuths.indexOf(process.env.BROADCASTER_USERNAME) === -1) {
       res.status(401).json({
         error: 'unauthorized_client',
-        error_description: `The app @${req.proxy} doesn't have permission to broadcast for @${req.user}`,
+        error_description: `Broadcaster account doesn't have permission to broadcast for @${req.user}`,
       });
     } else {
-      const appAccountAuths = accounts[0].posting.account_auths.map((account) => account[0]);
-      if (appAccountAuths.indexOf(process.env.BROADCASTER_USERNAME) === -1) {
-        res.status(401).json({
-          error: 'unauthorized_client',
-          error_description: `Broadcaster account doesn't have permission to broadcast for @${req.proxy}`,
-        });
-      } else {
-        next();
-      }
+      next();
     }
   }
 };
@@ -56,52 +46,20 @@ export const strategy = (req, res, next) => {
 
   if (token) {
     try {
-      const decoded = Buffer.from(b64uToB64(token), 'base64').toString();
-      const tokenObj = JSON.parse(decoded);
-      const signedMessage = tokenObj.signed_message;
-      if (
-        tokenObj.authors
-        && tokenObj.authors[0]
-        && tokenObj.signatures
-        && tokenObj.signatures[0]
-        && signedMessage
-        && signedMessage.type
-        && ['login', 'posting', 'offline', 'code', 'refresh']
-          .includes(signedMessage.type)
-        && signedMessage.app
-      ) {
-        const message = JSON.stringify({
-          signed_message: signedMessage,
-          authors: tokenObj.authors,
-          timestamp: tokenObj.timestamp,
-        });
-        const username = tokenObj.authors[0];
-        verify(message, username, tokenObj.signatures[0], (err, isValid) => {
-          if (!err && isValid) {
-            console.log(new Date().toISOString(), 'Token signature is valid', username);
-            let scope;
-            if (signedMessage.type === 'login') scope = ['login'];
-            if (['posting', 'offline', 'code', 'refresh']
-              .includes(signedMessage.type)) scope = authorized_operations;
-            let role = 'app';
-            if (signedMessage.type === 'code') role = 'code';
-            if (signedMessage.type === 'refresh') role = 'refresh';
-            /* eslint-disable no-param-reassign */
-            req.token = token;
-            req.role = role;
-            req.user = username;
-            req.proxy = signedMessage.app;
-            req.scope = scope;
-            req.type = 'signature';
-            /* eslint-enable no-param-reassign */
-          }
-          next();
-        });
-      } else {
-        next();
-      }
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        
+      /* eslint-disable no-param-reassign */
+      req.token = token;
+      req.role = 'app';
+      req.user = JSON.parse(decoded['data'])['username'];
+      req.proxy = decoded['data']['app'] || 'dapplr';
+      req.scope = config.authorized_operations;
+      req.type = 'signature';
+      /* eslint-enable no-param-reassign */
+
+      next();
     } catch (e) {
-      console.log(new Date().toISOString(), 'Token signature decoding failed', e);
+      console.log(new Date().toISOString(), 'Access Token decoding failed', e);
       next();
     }
   } else {
@@ -118,35 +76,6 @@ export const authenticate = (roles) => async (req, res, next) => {
       error: 'invalid_grant',
       error_description: 'The token has invalid role',
     });
-  } else if (['code', 'refresh'].includes(req.role)) {
-    let app;
-    try {
-      app = await getAppProfile(req.proxy);
-    } catch (e) {
-      console.error('Failed to get app profile', e);
-    }
-
-    const secret = req.query.client_secret || req.body.client_secret;
-    const secretHash = createHash('sha256').update(secret).digest('hex');
-
-    if (!app.secret || secretHash !== app.secret) {
-      res.status(401).json({
-        error: 'invalid_grant',
-        error_description: 'The code or secret is not valid',
-      });
-    } else if (app.allowed_ips && app.allowed_ips.length > 0) {
-      const reqIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-      if (intersection(app.allowed_ips, reqIp.replace(' ', '').split(',')).length > 0) {
-        next();
-      } else {
-        res.status(401).json({
-          error: 'unauthorized_access',
-          error_description: `The IP ${reqIp} is not authorized`,
-        });
-      }
-    } else {
-      next();
-    }
   } else {
     next();
   }
