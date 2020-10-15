79c8dfeeec23607f37219675f68348d873541f2b
Dinesh Yadav <dinesh.yadav@loconav.com>
2020-10-15 10:37:54 +0530

    Fixed Client

--- helpers/client.js
+++ helpers/client.js
@@ -3,3 +3,4 @@ import { Client } from '@hiveio/dhive';
 const client = new Client(process.env.BROADCAST_URL || 'https://api.hive.blog', { rebrandedApi: true });
 
 export default client;
+module.exports = client;
